31b86e46ee3fb0b5d5df29e49336c9f6494be7f4
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-08-03 21:55:58 +0500

    1.0

---
+++ 01_introduction_seaborn.py
@@ -0,0 +1,13 @@
+# ***********************Seaborn introduction****************************************
+# //////seaborn is the advance concept of the matplotlib 
+# /////we creat the graphs of high level as the help of seaborn 
+# /////seaorn is very helpful to plotting the graph
+
+# -------this is the link of seaborn official site
+# https://seaborn.pydata.org/
+
+# /////we installing the seaborn as the following code
+# pip install seaborn
+
+# /////for importing the seaborn we use this code
+import seaborn as sns
